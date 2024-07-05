62465b0afb50184f103135996063a8dd13b3d2af
jin <a11718trannhuy@gmail.com>
2024-07-05 09:16:38 +0700

    add loading page

---
+++ src/components/Layout/LoadingPage/index.tsx
@@ -0,0 +1,12 @@
+import IconBicycle from '@icons/iconBicycle.svg';
+import './style.scss';
+
+const LoadingPage = () => {
+    return (
+        <div className="loading-page">
+            <IconBicycle />
+        </div>
+    );
+};
+
+export default LoadingPage;
