aad91b833b53543f94e546470598fb6720c965e6
jin <a11718trannhuy@gmail.com>
2024-07-10 10:30:55 +0700

    mermaid sidebar setting popover

--- src/components/UI/MermaidSettingsPopover/index.tsx
+++ src/components/UI/MermaidSettingsPopover/index.tsx
@@ -1,7 +1,22 @@
+import { Switch, Button } from 'antd';
+
+import IconSync from '@icons/iconSync.svg';
+
 const MermaidSettingsPopover = () => {
     return (
-        <div className="">
-            MermaidSettingsPopover
+        <div className="flex flex-col gap-3 font-medium">
+            <div className='flex items-center w-full justify-between'>
+                <div className="">Auto sync</div>
+                <div className="">
+                    <Switch />
+                </div>
+            </div>
+            <Button
+                className='font-medium'
+                icon={<IconSync />}
+            >
+                Sync diagram
+            </Button>
         </div>
     );
 };
