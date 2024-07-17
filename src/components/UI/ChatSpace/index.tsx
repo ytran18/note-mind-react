a080b5fa1829edb0f47592bf0ae0ab3dd587dfd4
jin <a11718trannhuy@gmail.com>
2024-07-17 15:14:26 +0700

    update comming soon

--- src/components/UI/ChatSpace/index.tsx
+++ src/components/UI/ChatSpace/index.tsx
@@ -1,9 +1,14 @@
+import { Result } from 'antd';
 import IconCommingSoon from '@icons/iconCommingSoon.svg';
 
 const ChatSpace = () => {
     return (
         <div className="w-full h-full flex items-center justify-center">
-            <IconCommingSoon />
+            <Result
+                icon={<IconCommingSoon />}
+                title="Comming soon!"
+                subTitle="Chat with AI will comming soon!"
+            />
         </div>
     );
 };
--- src/components/UI/FlashCardSpace/index.tsx
+++ src/components/UI/FlashCardSpace/index.tsx
@@ -1,9 +1,14 @@
+import { Result } from 'antd';
 import IconCommingSoon from '@icons/iconCommingSoon.svg';
 
 const FlashCardSpace = () => {
     return (
         <div className="w-full h-full flex items-center justify-center">
-            <IconCommingSoon />
+            <Result
+                icon={<IconCommingSoon />}
+                title="Comming soon!"
+                subTitle="Flash card will comming soon!"
+            />
         </div>
     );
 };
